73119d1e9c2ed55bd9227bafdf54ed1651b0cc0b
kevin <kevin@tenxer.com>
2012-10-18 16:30:02 -0700

    Initial commit of the necessary PyDev files needed to remote debug.

---
+++ README.txt
@@ -0,0 +1,14 @@
+========
+PyDevSrc
+========
+
+PyDevSrc contains the necessary modules to enable remote debugging with the PyDev plugin of Eclipse.
+
+Usage
+=====
+
+Add this package to your project, and call::
+
+    from pydevsrc import pydevd
+    pydevd.settrace()
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PyDevSrc',
+    version='0.1.0',
+    author='Kevin Deggelman',
+    author_email='kevin@tenxer.com',
+    packages=['pydevsrc'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description="Pydev Source needed to remote debug in Eclipse",
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
